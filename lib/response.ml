3f45df602e4f166971ed60bb293777a76b515812
dx3mod <dx3mod@bk.ru>
2024-07-14 10:57:27 +0300

    Rename exception from Response module

--- lib/response.ml
+++ lib/response.ml
@@ -1,17 +1,17 @@
-type error = { error_code : int; error_msg : string }
+type error = { code : int; [@key "error_code"] msg : string [@key "error_msg"] }
 [@@deriving of_yojson { strict = false }]
 
-exception Response_error of error
+exception Api_error of error
 exception Parse_error of string
 
 let parse_json p input =
   match Yojson.Safe.from_string input with
   | `Assoc [ ("error", error) ] -> (
       match error_of_yojson error with
-      | Error err -> raise (Parse_error err)
-      | Ok resp_err -> raise (Response_error resp_err))
+      | Error parse_err -> raise (Parse_error parse_err)
+      | Ok api_err -> raise (Api_error api_err))
   | `Assoc [ ("response", response) ] -> (
       match p response with
-      | Error err -> raise (Parse_error err)
+      | Error parse_err -> raise (Parse_error parse_err)
       | Ok response -> response)
   | _ -> failwith "unknown response"
