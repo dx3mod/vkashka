d60a9f0fd498f9c489974a4fd3bf7d50d182a9f1
dx3mod <dx3mod@bk.ru>
2024-07-15 21:45:01 +0300

    Add video attachment

--- lib/models/media.ml
+++ lib/models/media.ml
@@ -5,13 +5,21 @@ module Photo = struct
   [@@deriving of_yojson { strict = false }]
 end
 
+module Video = struct
+  type t = { preview : image list [@key "image"] }
+  [@@deriving of_yojson { strict = false }]
+
+  and image = { height : int; width : int; url : string; with_padding : int }
+end
+
 module Attachment = struct
-  type t = Photo of Photo.t | Other of string
+  type t = Photo of Photo.t | Video of Video.t | Other of string
 
   let of_yojson : Yojson.Safe.t -> _ = function
     | `Assoc [ ("type", `String kind); (_, json) ] -> (
         match kind with
         | "photo" -> Photo.of_yojson json |> Result.map (fun p -> Photo p)
+        | "video" -> Video.of_yojson json |> Result.map (fun v -> Video v)
         | other -> Ok (Other other))
     | _ -> Error "invalid attachment"
 end
